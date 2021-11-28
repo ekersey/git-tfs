f92a16aed590c0530b440548e1670fbffd2fa4bd
ekersey <ekersey@users.noreply.github.com>
2021-11-28 01:21:30 -0500

    Rename default branch created to commit gitIgnore

--- src/GitTfs/Core/GitRepository.cs
+++ src/GitTfs/Core/GitRepository.cs
@@ -776,7 +776,7 @@ namespace GitTfs.Core
             Trace.WriteLine(".gitignore commit created: " + sha);
 
             _repository.Refs.Add(ShortToTfsRemoteName("default"), new ObjectId(sha));
-            _repository.Refs.Add(ShortToLocalName("master"), new ObjectId(sha));
+            _repository.Refs.Add(ShortToLocalName("Main"), new ObjectId(sha));
 
             return sha;
         }
