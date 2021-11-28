aa23efbc5cb6e8d0244d28c38488836a9209c7de
ekersey <ekersey@users.noreply.github.com>
2021-11-28 01:16:15 -0500

    Retry all requests to TFS when fail

--- src/GitTfs.VsCommon/Retry.cs
+++ src/GitTfs.VsCommon/Retry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using GitTfs.Core;
 
@@ -9,10 +10,10 @@ namespace GitTfs.VsCommon
     {
         public static void Do(Action action)
         {
-            Do(action, TimeSpan.FromSeconds(1));
+            Do(action, TimeSpan.FromSeconds(30));
         }
 
-        public static void Do(Action action, TimeSpan retryInterval, int retryCount = 10)
+        public static void Do(Action action, TimeSpan retryInterval, int retryCount = 100)
         {
             Do<object>(() =>
             {
@@ -23,10 +24,10 @@ namespace GitTfs.VsCommon
 
         public static T Do<T>(Func<T> action)
         {
-            return Do(action, TimeSpan.FromSeconds(1));
+            return Do(action, TimeSpan.FromSeconds(30));
         }
 
-        public static T Do<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 10)
+        public static T Do<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 100)
         {
             var exceptions = new List<Exception>();
 
@@ -38,16 +39,19 @@ namespace GitTfs.VsCommon
                 }
                 catch (Microsoft.TeamFoundation.TeamFoundationServerException ex)
                 {
+                    Trace.WriteLine($"{ex.Message}: Retry #{retry + 1}/{retryCount} in {retryInterval.TotalSeconds} seconds.");
                     exceptions.Add(ex);
                     Thread.Sleep(retryInterval);
                 }
                 catch (System.Net.WebException ex)
                 {
+                    Trace.WriteLine($"{ex.Message}: Retry #{retry + 1}/{retryCount} in {retryInterval.TotalSeconds} seconds.");
                     exceptions.Add(ex);
                     Thread.Sleep(retryInterval);
                 }
                 catch (GitTfsException ex) // allows continue of catch (MappingConflictException e) throw as innerexception
                 {
+                    Trace.WriteLine($"{ex.Message}: Retry #{retry + 1}/{retryCount} in {retryInterval.TotalSeconds} seconds.");
                     exceptions.Add(ex);
                     Thread.Sleep(retryInterval);
                 }
@@ -56,12 +60,12 @@ namespace GitTfs.VsCommon
             throw new AggregateException(exceptions);
         }
 
-        public static void DoWhile(Func<bool> action, int retryCount = 10)
+        public static void DoWhile(Func<bool> action, int retryCount = 100)
         {
-            DoWhile(action, TimeSpan.FromSeconds(0), retryCount);
+            DoWhile(action, TimeSpan.FromSeconds(30), retryCount);
         }
 
-        public static void DoWhile(Func<bool> action, TimeSpan retryInterval, int retryCount = 10)
+        public static void DoWhile(Func<bool> action, TimeSpan retryInterval, int retryCount = 100)
         {
             int count = 0;
             while (action())
@@ -69,6 +73,7 @@ namespace GitTfs.VsCommon
                 count++;
                 if (count > retryCount)
                     throw new GitTfsException("error: Action failed after " + retryCount + " retries!");
+                Trace.WriteLine($"DoWhile: Retry #{count}/{retryCount} in {retryInterval.TotalSeconds} seconds.");
                 Thread.Sleep(retryInterval);
             }
         }
